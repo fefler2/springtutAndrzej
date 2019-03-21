f8e01947c81f47726ce699f35d73a9aa122f29a5
giciarz412 <tomaszfefler1990@gmail.com>
2019-03-21 16:48:09 +0100

    a

--- src/main/java/andrzej/appdemo/AppdemoApplication.java
+++ src/main/java/andrzej/appdemo/AppdemoApplication.java
@@ -2,8 +2,11 @@ package andrzej.appdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
+@ComponentScan
 public class AppdemoApplication {
 
 	public static void main(String[] args) {
